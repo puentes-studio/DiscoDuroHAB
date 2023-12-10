c8080bd314c66c2d6b4f608178e448f5f1941e67
Scarlett <skarworking@hotmail.com>
2023-12-10 17:45:39 +0100

    Actualizacion de archivos

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DiscoDuroHAB
 Second Project of the Bootcamp of Programming and coding in Spain.
+
+- Instalar las dependencias mediante el comando "npm i" 
+  
+- Renombrar el fiechero ".env" y cubrir los campos. 
+  
+- Ejecutar el servidor con el comando "npm run dev"
---
+++ app.js
@@ -0,0 +1,22 @@
+// Importamos módulos
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Configuramos variables de entorno que nos permiten almacenar información confidencial como contraseñas sin comprometer la seguridad
+dotenv.config();
+
+// Creamos servidor con express
+const app = express();
+
+// Configuramos puerto
+const PORT = process.env.PORT || 3000;
+
+// Configuramos middleware
+app.use(cors());
+app.use(express.json());
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`¡La aplicación está lista y escuchando en el puerto ${PORT}!`);
+});
