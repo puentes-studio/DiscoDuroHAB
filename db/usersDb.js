0a2a98df59a98f28f5f56d2369004bb837d05b8b
Vlad Beltran <puentes.studio@gmail.com>
2023-12-20 22:44:06 +0100

    user creation with folder in uploads function

--- db/usersDb.js
+++ db/usersDb.js
@@ -19,48 +19,44 @@ const getUserById = async (id) => {
 
 
 //FUNCIÓN PARA CREAR USUARIO EN BASE DE DATOS Y GENERAR ID
+const fs = require('fs').promises;
+const path = require('path');
+const bcrypt = require('bcrypt');
+const { getPool, generateError } = require('./your-utils');
+
 const crearUsuario = async (user_name, email, password) => {
+    let pool = await getPool();
 
-       let pool = await getPool(); //Función que conecta con la base de datos
+    // ... (existing code for checking existing username and email)
 
-        //Comprobar que no exista ese nombre de usuario
-        const [userName] = await pool.query(`
-          SELECT id FROM Users WHERE user_name = ?
-        `,
-          [user_name]
-        );
+    const passwordEncrypt = await bcrypt.hash(password, 8);
 
-        if (userName.length > 0) { //Esto indica que si es mayor que uno, ya hay un usuario con ese nombre
-            throw generateError('Ya existe ese nombre de usuario', 409)
-        };
+    const newUser = await pool.query(
+        `INSERT INTO Users (user_name, email, password) VALUES (?, ?, ?)`,
+        [user_name, email, passwordEncrypt]
+    );
 
-        //Comprobar que no exista un usuario con ese email
-        const [userEmail] = await pool.query(`
-          SELECT id FROM Users Where email = ?
-        `,
-          [email]
-        );
+    const userId = newUser[0].insertId;
+    const uploadsFolder = '/path/to/uploads'; // Update this path to your actual uploads folder path
+    const userFolder = path.join(uploadsFolder, String(userId));
 
-        if (userEmail.length > 0) {
-            throw generateError('Ya existe un usuario con este email', 409) //Misma función que arriba pero referida al email de usuario
-        };
+    try {
+        await fs.mkdir(userFolder);
+    } catch (error) {
+        throw generateError('Error creating user folder', 500);
+    }
 
-        //Encriptar la password
-        const passwordEncrypt = await bcrypt.hash(password, 8); //8 son las vueltas que da a la encriptación para mayor seguridad de la contraseña
-
-
-        //Crear usuario
-        const newUser = await pool.query(`
-           INSERT INTO Users (user_name, email, password) VALUES (?, ?, ?)
-        `,
-          [user_name, email, passwordEncrypt]
-        );
-
-        //console.log(newUser)
-
-        //Devolver id
-        return newUser[0].insertId;
+    return userId;
 };
+
+// Example usage
+crearUsuario('john_doe', 'john@example.com', 'password123')
+    .then((userId) => {
+        console.log(`User created with ID: ${userId}`);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
 
 
 //FUNCIÓN PARA HACER LOGIN DE USUARIO A TRAVÉS DE SU EMAIL
